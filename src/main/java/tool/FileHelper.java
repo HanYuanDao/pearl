fe6335803b5607244aa204d6e2de5657ae5aca30
JasonHan <hanzhe.jason@gmail.com>
2018-05-08 17:04:32 +0800

    调整：新建工具类，存放对于文件处理的通用方法相关方法。例如读取保存。

---
+++ src/main/java/tool/FileHelper.java
@@ -0,0 +1,16 @@
+package tool;
+
+import java.io.FileInputStream;
+
+/**
+ * Desciption:
+ * Author: jasonhan.
+ * Creation time: 2018/5/7 4:56 PM.
+ * © Copyright 2013-2018.
+ */
+public class FileHelper {
+
+    private FileHelper() {}
+
+
+}
